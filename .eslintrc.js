91d86703a64f2f2c226243128ac000372a69cf23
Olivier.r <olivier.reboul@gmail.com>
2022-12-06 12:51:36 +0100

    fix: linter op

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,27 +5,20 @@ module.exports = {
         es2021: true,
         node: true,
     },
-    extends: airbnb - base,
+    extends: 'airbnb-base',
     overrides: [],
     parserOptions: {
-        ecmaVersion: latest,
+        ecmaVersion: 'latest',
     },
+    // dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
     rules: {
-        indent: [error, 4],
-        "max-len": [error, {
+        indent: ['error', 4],
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        'max-len': [
+            'error',
+            {
                 code: 110,
-                template: 80,
-                comments: 80,
-                ignorePattern: false,
-                ignoreComments: false,
-                ignoreTrailingComments: false,
                 ignoreUrls: true,
-                ignoreStrings: false,
-                ignoreTemplateLiterals: false,
-                ignoreRegExpLiterals: false,
-                ignoreHTMLAttributeValues: false,
-                ignoreHTMLTextContents: false,
-                tabWidth: 2,
             },
         ],
     },
