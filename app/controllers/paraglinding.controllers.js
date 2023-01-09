5157d3d0e6a4af61ad77d64953e8a8915fa16295
Olivier.r <olivier.reboul@gmail.com>
2023-01-09 23:21:16 +0100

    build: inistialize sqitch configuration

--- app/controllers/paraglinding.controllers.js
+++ app/controllers/paraglinding.controllers.js
@@ -4,6 +4,7 @@ export default{
         return res.send(`Controller2 paradilinding getone ${idParaglinding}`)
     },
     getAll : async(_req, res) => {
+        console.log("enculer")
         return res.send('controller2 paradilinding getAll')
     }
 };
