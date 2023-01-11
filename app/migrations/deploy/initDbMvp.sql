7e2f35b01cb70d9d93224749bb9e2cf98d977980
Olivier.r <olivier.reboul@gmail.com>
2023-01-11 14:48:43 +0100

    build: seeding mvp and create branche

--- app/migrations/deploy/initDbMvp.sql
+++ app/migrations/deploy/initDbMvp.sql
@@ -8,7 +8,7 @@ CREATE TABLE "paraglinding"(
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "name" TEXT NOT NULL UNIQUE,
     "type" TEXT,
-    "release_year" INTEGER NOT NULL,
+    "release_year" INTEGER,
     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
